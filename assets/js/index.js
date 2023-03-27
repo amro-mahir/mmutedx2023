6ae6ea308b1bdd3e9fb5ea677c105e6f8c3dcbf5
Amro Mahir Suliman Fadlelbari <amro@auronex.com>
2023-03-27 20:56:24 +0800

    tooltip added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,5 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const buyTicketBtn = document.getElementById('buy-ticket-btn')
+buyTicketBtn.setAttribute('disabled','')
