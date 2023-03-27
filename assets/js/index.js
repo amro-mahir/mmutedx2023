db547f3d940ccdb8a8dcae4537b98124e7e47382
Amro Mahir Suliman Fadlelbari <amro@auronex.com>
2023-03-27 17:12:15 +0800

    tooltip

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,2 @@
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
-
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
