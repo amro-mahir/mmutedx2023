032d523f5ea702d3f60f4187d52938c710a8457b
Amro Mahir Suliman Fadlelbari <amro@auronex.com>
2023-03-29 16:53:55 +0800

    js link to twitter page

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,11 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 document.getElementById('buy-ticket-btn').disabled = "true"
 document.getElementById('speaker-section').style.pointerEvents = "none"
 document.getElementById('speaker-section').style.filter = "blur(10px)"
+
+var elements = document.getElementsByClassName('twitter');
+for(var i=0; i<elements.length; i++) { 
+  elements[i].addEventListener('click', function() {
+    window.open("https://twitter.com/TEDxMMU2023")
+  })
+}
+
