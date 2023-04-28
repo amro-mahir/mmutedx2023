57eb938f169de53cce8dfd5508bd3f52a48e388d
Amro Mahir Suliman Fadlelbari <amro@auronex.com>
2023-04-29 01:30:03 +0800

    change link redirect

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-document.getElementById('buy-ticket-btn').disabled = "true"
+document.getElementById('buy-ticket-btn').href='https://linktr.ee/tedxmmu2023'
 document.getElementById('speaker-section').style.pointerEvents = "none"
 document.getElementById('speaker-section').style.filter = "blur(10px)"
 
